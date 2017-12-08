eff54943bfa55ba50e7f8ab0378836149e688aa6
kashdeya <kashdeya@outlook.com>
2017-12-07 19:15:47 -0600

    version update

--- src/main/java/com/kashdeya/trolloresreborn/ref/Reference.java
+++ src/main/java/com/kashdeya/trolloresreborn/ref/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
 	// Mod Constants
 		public static final String MOD_ID = "tor";
 		public static final String MOD_NAME = "trolloresreborn";
-		public static final String VERSION = "4.2.9";
+		public static final String VERSION = "4.2.11";
 		public static final String PREFIX_MOD = MOD_ID.toLowerCase() + ":";
 		
 		// Proxy Constants
